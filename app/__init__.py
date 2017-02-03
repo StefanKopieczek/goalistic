ea3f31a3f3dfbd2dbc3e90c73adac3a77befa57d
Stefan Kopieczek <stefankopieczek@gmail.com>
2017-02-03 11:25:56 +0000

    Add GET endpoint for user info.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import database
+
+
 app = Flask(__name__)
+db = database.DatabaseContext()
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/api/users/<user_id>')
+def get_user():
+    user = db.User.query.filter(User.id=user_id).one()
+    return jsonify({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+    })
