ed9249d4bd1acb41cab5207a07a7d47729977951
Stefan Kopieczek <stefankopieczek@gmail.com>
2017-02-05 16:43:07 +0000

    Get react up and running.
    Now to actually write the front-end!

--- react-src/webpack.config.js
+++ react-src/webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2016', 'react']
+                }
+            }
+        ]
+    },
 };
