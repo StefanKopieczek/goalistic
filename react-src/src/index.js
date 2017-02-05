7011b8e9f807a272c8ec21cd2f523069720d8b10
Stefan Kopieczek <stefankopieczek@gmail.com>
2017-02-05 17:50:06 +0000

    Template component using backend API
    Create a POC UserInfo application that hits the backend API.

--- react-src/src/index.js
+++ react-src/src/index.js
@@ -1,19 +1,61 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery')
 
-class Tracker extends React.Component {
+class App extends React.Component {
     render() {
         return (
-            <div className="tracker">
-                Tracker goes here!
+            <div className="app">
+                <UserInfo />
             </div>
         );
     }
 };
 
-loadTracker = function(elementId) {
+class UserInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'name': null,
+            'email': null,
+        };
+    }
+
+    componentDidMount() {
+        $.ajax({
+            url: "api/users/1",
+            dataType: 'json',
+            success: function(data) {
+                this.setState(data);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(status, err.toString());
+            }.bind(this)
+        });
+    }
+
+    render() {
+        if (this.state['name'] === null) {
+            return (
+                <div class="userinfo">
+                    <div><b>User information</b></div>
+                    Loading...
+                </div>
+            );
+        } else {
+            return (
+                <div class="userinfo">
+                    <div><b>User information</b></div>
+                    {this.state['name']} ({this.state['email']})
+                </div>
+            );
+        }
+    }
+}
+
+loadApp = function(elementId) {
     ReactDOM.render(
-        <Tracker />,
+        <App />,
         document.getElementById(elementId)
     );
 };
